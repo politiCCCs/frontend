65d9499d4b4553f721fc73594b017149790b310b
dalyIsaac <isaac.daly@outlook.com>
2021-05-10 11:06:50 +1200

    Fetch data

---
+++ src/state/data.ts
@@ -0,0 +1,82 @@
+/* eslint-disable no-param-reassign */
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+// Redux action helpers
+interface CouchDBRow<T> {
+	key: [string];
+	value: T;
+}
+
+interface CouchDBData<T> {
+	rows: CouchDBRow<T>[];
+}
+
+type LoadAction<T> = PayloadAction<CouchDBData<T>>;
+
+// Slice state
+interface Count {
+	sum: number;
+	count: number;
+	min: number;
+	max: number;
+	sumsqr: number;
+}
+
+interface PoliticianData {
+	likes?: Count;
+	retweets?: Count;
+	sentiment?: Count;
+}
+
+interface PoliticianDataState {
+	[user: string]: PoliticianData;
+}
+
+export interface DataState {
+	data: PoliticianDataState;
+}
+
+const initialState: DataState = { data: {} };
+
+// Load action payload into state
+const loadPoliticianPayload = <K extends keyof PoliticianData>(dataKey: K) => <
+	P extends PoliticianData[K]
+>(
+	state: DataState,
+	{ payload: { rows } }: LoadAction<P>,
+): void => {
+	for (const { key, value } of rows) {
+		const userId = key[0];
+
+		if (!Object.prototype.hasOwnProperty.call(state, userId)) {
+			state.data[userId] = {};
+		}
+
+		state.data[userId]![dataKey] = { ...value };
+	}
+};
+
+// Thunk
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const fetchData = (dataKey: keyof PoliticianData) =>
+	createAsyncThunk(`data/fetch/${dataKey}`, async () => {
+		const url = `/${dataKey}-per-politician`;
+		const response = await fetch(url);
+		return response.json();
+	});
+
+export const fetchLikes = fetchData("likes");
+export const fetchRetweets = fetchData("retweets");
+export const fetchSentiment = fetchData("sentiment");
+
+export const data = createSlice({
+	name: "data",
+	initialState,
+	reducers: {},
+	extraReducers: (builder) => {
+		builder
+			.addCase(fetchLikes.fulfilled, loadPoliticianPayload("likes"))
+			.addCase(fetchRetweets.fulfilled, loadPoliticianPayload("retweets"))
+			.addCase(fetchSentiment.fulfilled, loadPoliticianPayload("sentiment"));
+	},
+});
