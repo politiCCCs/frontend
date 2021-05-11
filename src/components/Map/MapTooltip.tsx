208d7bd965ff9206b7acbecf35ef03256213da96
dalyIsaac <isaac.daly@outlook.com>
2021-05-11 14:27:49 +1200

    Switched MapTooltip to CSS Modules

--- src/components/Map/MapTooltip.tsx
+++ src/components/Map/MapTooltip.tsx
@@ -1,4 +1,4 @@
-import "./MapTooltip.css";
+import styles from "./MapTooltip.module.css";
 import { Feature } from "./types";
 
 export interface MapTooltipProps {
@@ -11,7 +11,7 @@ export const MapTooltip = ({ feature, x, y }: MapTooltipProps): JSX.Element => {
 	const props = feature.properties;
 
 	return (
-		<div className="map-tooltip" style={{ left: x, top: y }}>
+		<div className={styles.mapTooltip} style={{ left: x, top: y }}>
 			<div>Electorate</div>
 			<div>{props.Elect_div}</div>
 
