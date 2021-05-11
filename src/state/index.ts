1aa3790f79754ad4bb321160d3d9dc4365203f18
dalyIsaac <isaac.daly@outlook.com>
2021-05-11 16:26:18 +1200

    Added names state

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { names } from "./names";
 
 import { twitterData } from "./twitterData";
 import { ui } from "./ui";
 
 export const store = configureStore({
 	reducer: {
+		names: names.reducer,
 		twitterData: twitterData.reducer,
 		ui: ui.reducer,
 	},
