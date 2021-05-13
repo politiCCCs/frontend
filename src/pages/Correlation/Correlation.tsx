81c167a93bd6707e9251802d86893dccc190cd17
dalyIsaac <isaac.daly@outlook.com>
2021-05-13 15:59:26 +1200

    Added extra scatterplots

--- src/pages/Correlation/Correlation.tsx
+++ src/pages/Correlation/Correlation.tsx
@@ -4,6 +4,9 @@ import { createSelector } from "../../state/selectors";
 
 export const CorrelationPage = (): JSX.Element => {
 	const sentimentVsVotes = useSelector(createSelector("sentiment"));
+	const tweetsVsVotes = useSelector(createSelector("count"));
+	const retweetsVsVotes = useSelector(createSelector("retweets"));
+	const likesVsVotes = useSelector(createSelector("likes"));
 
 	return (
 		<div>
@@ -12,6 +15,24 @@ export const CorrelationPage = (): JSX.Element => {
 				xAxisTitle="Sentiment"
 				yAxisTitle="Votes"
 				points={sentimentVsVotes}
+			/>
+			<ScatterPlot
+				name="Tweets"
+				xAxisTitle="Tweets"
+				yAxisTitle="Votes"
+				points={tweetsVsVotes}
+			/>
+			<ScatterPlot
+				name="Retweets"
+				xAxisTitle="Retweets"
+				yAxisTitle="Votes"
+				points={retweetsVsVotes}
+			/>
+			<ScatterPlot
+				name="Likes"
+				xAxisTitle="Likes"
+				yAxisTitle="Votes"
+				points={likesVsVotes}
 			/>
 		</div>
 	);
