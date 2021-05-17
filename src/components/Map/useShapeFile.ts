d53547f9be259f8cc2c851cef786bf79ed1cd2a3
dalyIsaac <isaac.daly@outlook.com>
2021-05-18 10:21:59 +1200

    Move fetching the shapefile to its own hook

---
+++ src/components/Map/useShapeFile.ts
@@ -0,0 +1,21 @@
+import { FeatureCollection } from "geojson";
+import { useEffect, useState } from "react";
+
+type SetError = (s: string) => void;
+
+export const useShapeFile = (setError: SetError): FeatureCollection | null => {
+	const [geoJSON, setGeoJSON] = useState<FeatureCollection | null>(null);
+
+	useEffect(() => {
+		fetch("/shapefile")
+			.then((res) => res.json())
+			.then((data) => {
+				setGeoJSON(data);
+			})
+			.catch(() => {
+				setError("Could not retrieve shape file");
+			});
+	}, [setError]);
+
+	return geoJSON;
+};
