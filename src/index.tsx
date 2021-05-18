64d13efd980d98065c7764c7c7568bda00d83a09
dalyIsaac <isaac.daly@outlook.com>
2021-05-19 10:12:04 +1200

    Fixed mapbox errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
-import "mapbox-gl/dist/mapbox-gl.css";
-import "./index.css";
-
 import React from "react";
 import ReactDOM from "react-dom";
+import mapboxgl from "mapbox-gl";
 import { Provider } from "react-redux";
 import { MapContext } from "react-map-gl";
+
 import { App } from "./App";
 import { store } from "./state";
 import { reportWebVitals } from "./reportWebVitals";
+
+import "mapbox-gl/dist/mapbox-gl.css";
+import "./index.css";
+
+// @ts-ignore
+// eslint-disable-next-line import/no-webpack-loader-syntax, import/first, @typescript-eslint/no-unsafe-member-access
+mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
 ReactDOM.render(
 	<React.StrictMode>
